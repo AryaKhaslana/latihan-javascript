31d74843551ea171fa507e4df2b4e0aedab291ae
arya <aryachakabrajawisesa@gmail.com>
2025-10-27 12:52:35 +0700

    penambahan js

--- Projek JS/script.js
+++ Projek JS/script.js
@@ -1,2 +1,23 @@
-
+document.addEventListener("DOMContentLoaded", () => {
+    const elementsToAnimate = document.querySelectorAll('.parent');
   
+    // 2. Siapin si 'mata-mata' (Observer)
+    const observer = new IntersectionObserver((entries) => {
+      // 'entries' itu daftar elemen yg lagi diamatin
+      entries.forEach(entry => {
+        
+        // 3. KALO elemennya kelihatan di layar...
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible'); // Tambahin class .visible
+          observer.unobserve(entry.target); // Berhenti ngamatin (biar hemat)
+        }
+      });
+    }, {
+      threshold: 0.1 // Animasi jalan pas 10% elemennya kelihatan
+    });
+  
+    // 4. Suruh si 'mata-mata' buat ngamatin semua elemen kita
+    elementsToAnimate.forEach(el => {
+      observer.observe(el);
+    });
+  });
