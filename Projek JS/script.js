0697b451c043a8dbd5df5a95afdea5c1d4259402
arya <aryachakabrajawisesa@gmail.com>
2025-11-02 13:44:51 +0700

    Done projek js

--- Projek JS/script.js
+++ Projek JS/script.js
@@ -39,6 +39,26 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
+  document.addEventListener("DOMContentLoaded", () => {
+    const elementsToAnimate = document.querySelectorAll('.main-info');
+  
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible'); 
+          observer.unobserve(entry.target); 
+        }
+      });
+    }, {
+      threshold: 0.1 
+    });
+  
+    elementsToAnimate.forEach(el => {
+      observer.observe(el);
+    });
+  });
+
 //  sebagai pengaktif scroller
 const swiper = new Swiper('.mySwiper', {
   
